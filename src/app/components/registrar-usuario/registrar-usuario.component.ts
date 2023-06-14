3f324367b8f6322135d2bb31876327ca5109c228
PatricioNorambuena <patoosb7@gmail.com>
2023-06-14 06:51:57 -0400

    cuarta actualizacion

--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -12,10 +13,10 @@ export class RegistrarUsuarioComponent implements OnInit {
   registrarUsuario: FormGroup;
 
   constructor(private fb: FormBuilder,
-      private afAuth: AngularFireAuth) {
+      private afAuth: AngularFireAuth, private toastr: ToastrService) {
     this.registrarUsuario = this.fb.group({
       nombre: ['', Validators.required],
-      telefono: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+      telefono: ['', [Validators.required, Validators.pattern('[0-9]+'), this.validarTelefono]],
       direccion: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
@@ -43,33 +44,52 @@ export class RegistrarUsuarioComponent implements OnInit {
     const password = this.registrarUsuario.value.password;
     const repetirPassword = this.registrarUsuario.value.repetirPassword;
     
-    this.afAuth.createUserWithEmailAndPassword(email, password).then((user) => {
+    this.afAuth
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
       console.log(user);
-    }).catch((error) => {
+    })
+    .catch((error) => {
       console.log(error);
-      alert(this.firebaseError(error.code));
+      this.toastr.error(this.firebaseError(error.code), 'Error');
     })
   }
+
   firebaseError (code: string) {
     switch(code) {
       case 'auth/email-already-in-use':
-        return 'El usuario ya existe';
+        return 'La cuenta que intenta crear ya existe. Por favor, introduzca un nuevo correo o intente recuperar su contraseña.';
+      case 'auth/weak-password':
+        return 'La contraseña es demasiado debil. Por favor, introduzca una nueva contraseña con almenos 6 carácteres.';
+      case 'auth/invalid-email':
+        return 'Por favor, introduzca un correo válido.';
+      case 'auth/missing-password':
+        return 'Por favor, introduzca una contraseña.';
       default:
         return 'Error desconocido';
     }
 
   }
-
+  
   // Validador personalizado para verificar si las contraseñas coinciden
   passwordMatchValidator(control: AbstractControl): { [key: string]: any } | null {
     const password = control.get('password');
     const repetirPassword = control.get('repetirPassword');
-
+    
     if (password && repetirPassword && password.value !== repetirPassword.value) {
       return { passwordMismatch: true };
     }
-
+    
     return null;
   }
-
+  
+  // Validar telefono
+  validarTelefono(control: AbstractControl): { [key: string]: any } | null {
+    const telefono = control.value;
+    if (telefono && telefono.length !== 8) {
+      return { telefonoInvalido: true };
+    }
+    return null;
+  }
+  
 }
