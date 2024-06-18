f7744aed04248fcf2de775143529b38dc1f9a7bf
Diego <diegojb137@gmail.com>
2024-06-18 09:30:42 -0400

    se creo tabla para listar los cortes y preparaciones, se creo box component para el filtro, y se agrego paginacion

--- src/app/components/productos/pollo/pollo.component.ts
+++ src/app/components/productos/pollo/pollo.component.ts
@@ -11,8 +11,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PolloComponent implements OnInit {
   cortes: Corte[] = [];
   filteredCortes: Corte[] = [];
+  paginatedCortes: Corte[] = [];
   searchQuery: string = '';
   selectedPreparation: string = '';
+  currentPage: number = 1;
+  itemsPerPage: number = 10; // Mostrar 10 elementos por página
+  totalPages: number = 1;
+  tableHeight: any;
 
   constructor(
     private carneService: CarneService,
@@ -24,6 +29,7 @@ export class PolloComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       this.searchQuery = params['search'] || '';
       this.selectedPreparation = params['preparation'] || '';
+      this.currentPage = +params['page'] || 1;
       this.loadCortes();
     });
   }
@@ -43,14 +49,47 @@ export class PolloComponent implements OnInit {
         : true;
       return matchesName && matchesPreparation;
     });
+    this.totalPages = Math.ceil(this.filteredCortes.length / this.itemsPerPage);
+    this.paginateCortes();
+  }
+
+  paginateCortes(): void {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedCortes = this.filteredCortes.slice(start, end);
   }
 
   onSearch(): void {
+    this.currentPage = 1;
+    this.updateQueryParams();
+    this.filterCortes();
+  }
+
+  updateQueryParams(): void {
     this.router.navigate([], {
-      queryParams: { search: this.searchQuery, preparation: this.selectedPreparation },
+      queryParams: {
+        search: this.searchQuery,
+        preparation: this.selectedPreparation,
+        page: this.currentPage
+      },
       queryParamsHandling: 'merge'
     });
-    this.filterCortes();
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateQueryParams();
+      this.paginateCortes();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateQueryParams();
+      this.paginateCortes();
+    }
   }
 
   goBack(): void {
